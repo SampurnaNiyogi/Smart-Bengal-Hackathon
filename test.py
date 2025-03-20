57d24fae15a57fa274371220bc066d3abe261ecc
cubexgani <mroybros30@gmail.com>
2025-03-20 23:39:37 +0530

    Better, easier setup

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+from dotenv import load_dotenv
+import os
+# Load environment variables from .env
+load_dotenv()
+
+# Get the JSON key path
+service_account_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+
+if not service_account_path:
+    raise ValueError("GOOGLE_APPLICATION_CREDENTIALS is not set in the .env file")
 
 try:
     print("Initializing Firebase...")
 
     # Check if Firebase is already initialized
     if not firebase_admin._apps:
-        cred = credentials.Certificate("/Users/adityadebnath/Documents/sbh25-2d8ba-firebase-adminsdk-fbsvc-fc8ef9b782.json ")
+        cred = credentials.Certificate(service_account_path)
         firebase_admin.initialize_app(cred)
 
     db = firestore.client()
